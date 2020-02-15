fbae9d9ba7b804fba07cb9409fcb42104a6f5836
Mohammad Wahyu Kurniawan <wahyukur81@gmail.com>
2020-02-15 15:42:41 +0700

    fix merge

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -147,9 +147,6 @@
     	<i class="fas fa-angle-up"></i>
     </a>
 
-	<!-- Bootstrap core JavaScript-->
-	<script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js') ?>"></script>
-  	<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
     <!-- Core plugin JavaScript-->
     <script src="<?php echo base_url('assets/vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
     <!-- Custom scripts for all pages-->
